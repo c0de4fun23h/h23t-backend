1f839ef2a4c1411743895f5d6b80e815041c3e93
Kefa Vova <kefavova24@gmail.com>
2023-05-17 02:38:48 +0300

    initial commit

---
+++ src/main/java/ua/c0de4fun/backend/web/HelloController.java
@@ -0,0 +1,14 @@
+package ua.c0de4fun.backend.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
